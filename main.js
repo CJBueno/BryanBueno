079e3bb6279565d38df5f9055960d2e1e3097b00
Christian J Bueno <christianjonbueno@me.com>
2018-11-02 11:13:45 -0700

    pages, photos,colors

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 //   var collapsibleElem = document.querySelector('.collapsible');
 //   var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+baguetteBox.run('.tz-gallery');
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
