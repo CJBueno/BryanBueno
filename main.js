0cb999946d00dde4de682f8240c7b0b8adf41b9d
Christian J Bueno <christianjonbueno@me.com>
2018-11-16 12:01:34 -0800

    Background video

--- main.js
+++ main.js
@@ -11,3 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+var video = document.getElementById("myVideo");
+video.playbackRate = 3.0
